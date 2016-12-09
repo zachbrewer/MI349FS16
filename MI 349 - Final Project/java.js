6889f0cdbc9f9101ccd6257ca7052c764e95b22b
zachbrewer <brewerza@msu.edu>
2016-12-09 18:52:44 -0500

    Final Project - All Requirements

---
+++ MI 349 - Final Project/java.js
@@ -0,0 +1,6 @@
+function hello () {alert("Cooper Manning - cooperthemanning69@gmail.com");}
+function cool () {alert("Cooper Manning - cooperthemanning69@gmail.com (989)414-0069");}
+function cool2 () {alert("Greg Odin - choosingnottoplay@yahoo.com (281)330-8004 ");}
+function cool3 () {alert("Michael Jones - notthatmikejones@hotmail.com (281)330-8004 )");}
+function signin () {alert("Sign In feature coming soon. Stay posted!");}
+function signup () {alert("Sign Up feature coming even sooner. It's going to be sweet.");}
